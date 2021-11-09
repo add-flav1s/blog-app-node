f45de12446569ee3d646e366b674c1ccfd8ca222
add-flav1s <aflavia17_@hotmail.com>
2021-11-09 08:25:35 -0300

    Configurando os modulos.

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ const app = express();
 
 // ----- Configuracoes ----- //
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 // ----- Rotas ----- //
 
