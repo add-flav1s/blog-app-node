32c1c46424b3690576808e192c714e8658537aac
add-flav1s <aflavia17_@hotmail.com>
2021-11-14 13:54:03 -0300

    Listando as categorias e editando com o bootstrap.

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,14 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/categorias', (req, res) => {
-    res.render('admin/categorias');
+    Categoria.find().then((categorias) => {
+        res.render('admin/categorias', {
+            categorias: categorias.map(categorias => categorias.toJSON())
+        })
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao listar as categorias');
+        res.redirect('/admin');
+    });
 });
 
 router.get('/categorias/add', (req, res) => {
