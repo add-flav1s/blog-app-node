2997780f5c97f4ba4085a8a45684ad19c873687e
add-flav1s <aflavia17_@hotmail.com>
2021-11-14 12:44:41 -0300

    Criando a validacao de dados e exibindo mensagens de sucesso e erro.

--- app.js
+++ app.js
@@ -6,15 +6,37 @@ const app = express();
 const admin = require('./routes/admin');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 
-// ----- Configuracoes ----- //
+// ----- Configuracoes dos modulos ----- //
 
+// Configurando o session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
+
+// Configurando o flash
+app.use(flash());
+
+//Criando um middleware 
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+// Configurando o express
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
 
+// Configurando o handlebars
 app.engine('handlebars', handlebars({
     extname: 'handlebars',
     defaultLayouts: 'main',
@@ -22,8 +44,10 @@ app.engine('handlebars', handlebars({
 }))
 app.set('view engine', 'handlebars')
 
+// Configurando o uso do bootstrap
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Configurando o mongoose
 mongoose.connect('mongodb://localhost/db_blogapp').then(() => {
     console.log("Conectado ao banco de dados!")
 }).catch((err) => {
